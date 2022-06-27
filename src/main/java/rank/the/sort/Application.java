53ae6800176ff4b89919267203ca2e603bb98e7f
Ricardo Mendes <mendes.r@gmx.com>
2022-06-27 20:37:37 +0100

    CREATE starting files for java application

---
+++ src/main/java/rank/the/sort/Application.java
@@ -0,0 +1,7 @@
+package rank.the.sort;
+
+class Application {
+
+  public static void main(String[] args) {}
+
+}
---
+++ src/test/java/rank/the/sort/ApplicationTest.java
@@ -0,0 +1,18 @@
+package rank.the.sort;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+    @Test
+    void testMain() {
+        // arrange
+
+        // act
+
+        // assert
+        assertTrue(true);
+    }
+
+}
